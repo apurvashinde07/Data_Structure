e251b5f765937070e1b998b6da242b2afe8083ed
Apurva shinde <apurvashinde077@gmail.com>
2026-02-04 22:53:56 +0530

    Add files via upload

---
+++ Searching Technique/linearsearch.cpp
@@ -0,0 +1,22 @@
+////Linear search 
+#include<iostream>
+using namespace std;
+int linearsearch(int arr[], int size, int target){
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i]== target)
+        {
+          return i;  
+        }    
+    }
+    return -1;  //not found
+    
+}
+int main(){
+    int arr[] = {5, 6, 7, 3, 8, 3};
+    int size = 6;
+    int target = 3;
+
+    cout<<linearsearch(arr, size, target)<<endl;
+    return 0;
+}
